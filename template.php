3c3834087d0ed0e9c100913f652d014b57c8802e
Indra Sutriadi Pipii <indra.sutriadi@gmail.com>
2011-01-11 19:09:28 +0000

    Commit master

--- template.php
+++ template.php
@@ -1,13 +1,23 @@
 <?php
 /*
- *
- * SMember Plugin
- * Copyright (c) 1431 H / 2010 M
- * by Indra Sutriadi Pipii (indra.sutriadi@gmail.com)
- *
- * @file: template.php
- * @desc: template page
- *
+ *      template.php
+ *      
+ *      Copyright 2011 Indra Sutriadi Pipii <indra.sutriadi@gmail.com>
+ *      
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *      
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *      
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *      MA 02110-1301, USA.
  */
 
 if ( ! defined('SENAYAN_BASE_DIR')) { exit(); }
@@ -39,7 +49,7 @@ if (!$can_read)
 			<label for="theme"><u>T</u>heme:</label>
 			<?php echo $optstyles;?>
 		</div>
-		<h1>SMember v 0.1</h1>
+		<h1>SMember v 0.2</h1>
 		<div id="demo">
 			<form id="formulir" name="formulir" target="" action="" method="POST">
 				<div style="text-align:left; padding-bottom: 1em; float: left;" class="ui-widget">
@@ -105,7 +115,7 @@ if (!$can_read)
 			</button>
 		</div>
 		<address style="text-align: center;">
-			Copyright &copy; 1431 H / 2010 M by Indra Sutriadi Pipii.<br />
+			Copyright &copy; 1431 - 1432 H / 2010 - 2011 M by Indra Sutriadi Pipii.<br />
 			Build with jQuery-UI + dataTables plugin.
 		</address>
 	</div>
